e28df800b36a69f8c3e49250781aab51cf53202a
yj-hong96 <yj.hong960906@gmail.com>
2025-04-23 10:52:23 +0900

    java project

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+ * javac HelloWorld.java <- 오른쪽 마우스 실행
+ * java Helloworld < 왼쪽 파일탐색기에서 실행
+ * Run 실행 단축기 ctrl +F11
+*/
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello Java!!");
+	}
+}
